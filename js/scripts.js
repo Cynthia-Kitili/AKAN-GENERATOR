fb9a6044955e043ec360d15b4241a4ffbe5cbad5
Cynthia Kitili <nyambucindy0@gmail.com>
2020-10-04 18:23:13 +0300

    add flows and operators

--- js/scripts.js
+++ js/scripts.js
@@ -19,5 +19,53 @@ form.addEventListener("submit", (e) => {
   var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua","Ama"];
   var  maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi","Kwame"];
   var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  
+
+  if (dayOfTheWeek == 0 && gender == "female") {
+    akanName = femaleAkanNames[0];
+} else if (dayOfTheWeek == 0 && gender == "male") {
+    akanName = maleAkanNames[0];
+
+
+} else if (dayOfTheWeek == 1 && gender == "female") {
+    akanName = femaleAkanNames[1];
+} else if (dayOfTheWeek == 1 && gender == "male") {
+    akanName = maleAkanNames[1];
+
+
+} else if (dayOfTheWeek == 2 && gender == "female") {
+    akanName = femaleAkanNames[2];
+} else if (dayOfTheWeek == 2 && gender == "male") {
+    akanName = maleAkanNames[2];
+
+
+} else if (dayOfTheWeek == 3 && gender == "female") {
+    akanName = femaleAkanNames[3];
+} else if (dayOfTheWeek == 3 && gender == "male") {
+    akanName = maleAkanNames[3];
+
+
+} else if (dayOfTheWeek == 4 && gender == "female") {
+   akanName = femaleAkanNames[4];
+} else if (dayOfTheWeek == 4 && gender == "male") {
+    akanName = maleAkanNames[4];
+
+
+} else if (dayOfTheWeek == 5 && gender == "female") {
+   akanName = femaleAkanNames[5];
+} else if (dayOfTheWeek == 5 && gender == "male") {
+    akanName = maleAkanNames[5];
+
+
+} else if (dayOfTheWeek == 6 && gender == "female") {
+    akanName = femaleAkanNames[6];
+} else if (dayOfTheWeek == 6 && gender == "male") {
+    akanName = maleAkanNames[6];
+}
+
+var result = document.getElementById("reveal-result").innerHTML = "You were born on a " + bornDay + " so your akan name is " + akanName + "!"
+document.getElementById("akanForm").reset();
+});
+
+
+
 
