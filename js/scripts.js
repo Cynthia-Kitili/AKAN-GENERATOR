aebd4d39444c19f85094d104a43c12ea086d2d3b
Cynthia Kitili <nyambucindy0@gmail.com>
2020-10-04 18:15:56 +0300

    add date,month,year,century

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 let form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  var formData = function() {
+    var date = parseInt( document.getElementById("Form").elements.namedItem("date").value );
+    var month = parseInt(document.getElementById("Form").elements.namedItem("month").value );
+    var year = parseInt(document.getElementById("Form").elements.namedItem("year").value);
+    var century=parseInt(document.getElementById("Form").elements.namedItem("century").value);
+    var gender = document.getElementById("Form").elements.namedItem("gender")
+      .value;  
