9401e3c8d7d5cd46083da3a8d85be6ca70efacd6
Cynthia Kitili <nyambucindy0@gmail.com>
2020-10-05 10:23:46 +0300

    add chnges made to html and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,130 @@
-let form = document.getElementById("form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  var formData = function() {
-    var date = parseInt( document.getElementById("Form").elements.namedItem("date").value );
-    var month = parseInt(document.getElementById("Form").elements.namedItem("month").value );
-    var year = parseInt(document.getElementById("Form").elements.namedItem("year").value);
-    var century=parseInt(document.getElementById("Form").elements.namedItem("century").value);
-    var gender = document.getElementById("Form").elements.namedItem("gender")
-      .value;  
-    
-      CC = century
-      YY = year
-      MM = month
-      DD = date
-      dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;    
-      var weekDay = dayOfTheWeek.getDay(); 
+var formData = function() {
+  var date = parseInt(
+    document.getElementById("Form").elements.namedItem("date").value
+  );
+  var month = parseInt(
+    document.getElementById("Form").elements.namedItem("month").value
+  );
+  var year = parseInt(
+    document.getElementById("Form").elements.namedItem("year").value
+  );
+  var gender = document.getElementById("Form").elements.namedItem("gender")
+    .value;
+  var dateOfBirth = new Date(year + "/" + month + "/" + date);
+  var weekDay = dateOfBirth.getDay();
+  
+  if (date < 1 || date > 31) {
+    return (document.getElementById("error").innerHTML =
+      "Invalid Data");
+  }if (month < 1 || month > 12 || (month == 2 && date > 29)) {
+    return (document.getElementById("error").innerHTML =
+      "Invalid  Data");
+  
+  } else if (year === NaN || year ==="") {
+    document.getElementById("error").innerHTML =
+      "Invalid Data";
 
-  var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua","Ama"];
-  var  maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi","Kwame"];
-  var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-  if (dayOfTheWeek == 0 && gender == "female") {
-    akanName = femaleAkanNames[0];
-} else if (dayOfTheWeek == 0 && gender == "male") {
-    akanName = maleAkanNames[0];
-
-
-} else if (dayOfTheWeek == 1 && gender == "female") {
-    akanName = femaleAkanNames[1];
-} else if (dayOfTheWeek == 1 && gender == "male") {
-    akanName = maleAkanNames[1];
+  }
 
 
-} else if (dayOfTheWeek == 2 && gender == "female") {
-    akanName = femaleAkanNames[2];
-} else if (dayOfTheWeek == 2 && gender == "male") {
-    akanName = maleAkanNames[2];
+  var maleAkan = ["Kwasi","Kwadwo", "Kwabena", "Kwaku","Yaw","Kofi", "Kwame"];
+
+  var femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+  var daysOfTheWeek = ["Sunday","Monday","Tuesday", "Wednesday", "Thursday", "Friday","Saturday" ];
+
+  var association=["Universe","Peace","Ocean","Spider","Earth","Fertility","God"];
+
+  if (weekDay === 0 && gender === "male") {
+    return (document.getElementById("alert").innerHTML =
+      "You were born on a : " +
+      daysOfTheWeek[weekDay] +
+      ("  .Your Akan name is : " + maleAkan[weekDay])+ (".It is associated with: " +association[0]));
+
+  }else if (weekDay === 0 && gender === "female") {
+    return (document.getElementById("alert").innerHTML =
+    "You were born on a : " +
+      daysOfTheWeek[weekDay] +
+      ("  .Your Akan name is : " + femaleAkan[weekDay])+ (".It is associated with: " +association[0]));
+           
+
+  } else if (weekDay === 1 && gender === "male") {
+    return (document.getElementById("alert").innerHTML =
+    "You were born on a : " +
+    daysOfTheWeek[weekDay] +
+    ("  .Your Akan name is : " + maleAkan[weekDay])+ (".It is associated with: " +association[1]));
+
+  }else if (weekDay === 1 && gender === "female") {
+    return (document.getElementById("alert").innerHTML =
+    "You were born on a : " +
+      daysOfTheWeek[weekDay] +
+      ("  .Your Akan name is : " + femaleAkan[weekDay])+ (".It is associated with: " +association[1]));
+
+  } else if (weekDay === 2 && gender === "male") {
+    return (document.getElementById("alert").innerHTML =
+    "You were born on a : " +
+    daysOfTheWeek[weekDay] +
+    ("  .Your Akan name is : " + maleAkan[weekDay])+ (".It is associated with: " +association[2]));
+
+  }else if (weekDay === 2 && gender === "female") {
+    return (document.getElementById("alert").innerHTML =
+    "You were born on a : " +
+      daysOfTheWeek[weekDay] +
+      ("  .Your Akan name is : " + femaleAkan[weekDay])+ (".It is associated with: " +association[2]));  
 
 
-} else if (dayOfTheWeek == 3 && gender == "female") {
-    akanName = femaleAkanNames[3];
-} else if (dayOfTheWeek == 3 && gender == "male") {
-    akanName = maleAkanNames[3];
+  } else if (weekDay === 3 && gender === "male") {
+    return (document.getElementById("alert").innerHTML =
+    "You were born on a : " +
+    daysOfTheWeek[weekDay] +
+    ("  .Your Akan name is : " + maleAkan[weekDay])+ (".It is associated with: " +association[3]));
+     
+
+  }else if (weekDay === 3 && gender === "female") {
+    return (document.getElementById("alert").innerHTML =
+      "You were born on a : " +
+      daysOfTheWeek[weekDay] +
+      ("  .Your Akan name is : " + femaleAkan[weekDay])+ (".It is associated with: " +association[3]));
 
 
-} else if (dayOfTheWeek == 4 && gender == "female") {
-   akanName = femaleAkanNames[4];
-} else if (dayOfTheWeek == 4 && gender == "male") {
-    akanName = maleAkanNames[4];
+  } else if (weekDay === 4 && gender === "male") {
+    return (document.getElementById("alert").innerHTML =
+    "You were born on a : " +
+    daysOfTheWeek[weekDay] +
+    ("  .Your Akan name is : " + maleAkan[weekDay])+ (".It is associated with: " +association[4]));
+
+  }else if (weekDay === 4 && gender === "female") {
+    return (document.getElementById("alert").innerHTML =
+      "You were born on a : " +
+      daysOfTheWeek[weekDay] +
+      ("  .Your Akan name is : " + femaleAkan[weekDay])+ (".It is associated with: " +association[4]));
+
+  } else if (weekDay === 5 && gender === "male") {
+    return (document.getElementById("alert").innerHTML =
+    "You were born on a : " +
+    daysOfTheWeek[weekDay] +
+    ("  .Your Akan name is : " + maleAkan[weekDay])+ (".It is associated with: " +association[5]));
+     
+
+  }else if (weekDay === 5 && gender === "female") {
+    return (document.getElementById("alert").innerHTML =
+      "You were born on a : " +
+      daysOfTheWeek[weekDay] +
+      ("  .Your Akan name is : " + femaleAkan[weekDay])+ (".It is associated with: " +association[5]));
 
 
-} else if (dayOfTheWeek == 5 && gender == "female") {
-   akanName = femaleAkanNames[5];
-} else if (dayOfTheWeek == 5 && gender == "male") {
-    akanName = maleAkanNames[5];
+  } else if (weekDay === 6 && gender === "male") {
+    return (document.getElementById("alert").innerHTML =
+    "You were born on a : " +
+    daysOfTheWeek[weekDay] +
+    ("  .Your Akan name is : " + maleAkan[weekDay])+ (".It is associated with: " +association[6]));
 
+  }else if (weekDay === 6 && gender === "female") {
+    return (document.getElementById("alert").innerHTML =
+      "You were born on a : " +
+      daysOfTheWeek[weekDay] +
+      ("  .Your Akan name is : " + femaleAkan[weekDay])+ (".It is associated with: " +association[6]));
 
-} else if (dayOfTheWeek == 6 && gender == "female") {
-    akanName = femaleAkanNames[6];
-} else if (dayOfTheWeek == 6 && gender == "male") {
-    akanName = maleAkanNames[6];
+  
+  } 
 }
-
-var result = document.getElementById("reveal-result").innerHTML = "You were born on a " + bornDay + " so your akan name is " + akanName + "!"
-document.getElementById("akanForm").reset();
-});
-
-
-
-
