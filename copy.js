05a8943dd92f1c76e489b88db0847b71b0fe481a
Cynthia Kitili <nyambucindy0@gmail.com>
2020-10-09 17:04:32 +0300

    add all commits

--- copy.js
+++ copy.js
@@ -1,19 +1,103 @@
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
 
+function date() {
+  var date = document.getElementById("date").value;
+  if (date == "") {
+    alert("Please fill in the Date ");
+    return false;
+  }
+}
+function month() {
+  var month = document.getElementById("month").value;
+  if (month == "0") {
+    alert("PLease fill in the Month");
+    return false;
+  }
+}
+function showYear() {
+  var year = document.getElementById("year").value;
+  if (year == "") {
+    alert("PLease fill in the Year");
+    return false;
+  }
+}
+function century() {
+  var century = document.getElementById("century").value;
+  if (century == "") {
+    alert("Please fill in the Century");
+    return false;
+  }
+}
+
+function gender() {
+  var gender = document.getElementById("gender").value;
+  if (gender == "1") {
+    alert("Please select gender");
+    return false;
+  }
+}
+
+
+function allLetters(input){
+  var characters = /[^a-z]/gi;
+  input.value = input.value.replace(characters, "");
+}
+
+function digits(input){
+  var digits = /[^0-9]/;
+  input.value = input.value.replace(digits, "");
+}
+
+function year(){
+  var year = document.getElementById('year').value;
+  if (year < 1899 || year > currentYear) {
+    alert("Invalid Year");
+    return false;
+  }
+}
+
+function leapYear(){
+  var monthsWithThirtyOneDays = [1, 3, 5, 7, 9, 10, 12];
+  var monthNumber = parseInt(document.getElementById("month").value);
+  var dayNumber = parseInt(document.getElementById("date").value);
+  var yearNumber = parseInt(document.getElementById("year").value);
+  var yearNumber = parseInt(document.getElementById("century").value);
+  var first= yearNumber % 100;
+  var second= yearNumber % 400;
+  var third= yearNumber % 4;
+  var fourth= monthsWithThirtyOneDays.includes(monthNumber);
+
+  if (monthNumber === 2 && dayNumber > 28 && first=== 0 && second!== 0){
+    alert("Invalid day: The entered year, February had 28 days.");
+    return false;
+  }
+  else if (monthNumber === 2 && dayNumber > 28 && third!== 0) {
+    alert("Invalid day: The entered year, February had 28 days.");
+    return false;
+  }
+  else if (!fourth && dayNumber > 30) {
+    alert("Invalid day: The selected month has 30 days");
+    return false;
+  }
+  else if (dayNumber > 31 || dayNumber < 1) {
+    alert("Invalid day: Months have a possible 1 to 31 days");
+    return false;
+  }
+}
 
 function formData(){
   var  daysOfTheWeek= ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
   var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
   var association=["Universe","Peace","Ocean","Spider","Earth","Fertility","God"];
-  var cc = parseInt(document.getElementById("century").value.slice(0,2));
-  var yy = parseInt(document.getElementById("born").value.slice(2,4));
-  var dd = parseInt(document.getElementById("born").value);
-  var mm = parseInt(document.getElementById("born").value);
-  var method =  ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )%7
+  var cc = parseInt(document.getElementById("century").value.slice(0, 2));
+  var yy = parseInt(document.getElementById("year").value.slice(2, 4));
+  var dd = parseInt(document.getElementById("day").value);
+  var mm = parseInt(document.getElementById("month").value);
+  var method = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
   var weekDay = daysOfTheWeek[method.toFixed()];
   var gender = parseInt(document.getElementById("gender").value);
-  alert("confirm you have filled in all data");
- 
   
   if (weekDay === 'Sunday'&&gender == 2) {
     document.getElementById("output").value = "You were born on Sunday: Your Akan Name is: " + akanMale[0] + ".It is associated with: "+ association[0];
@@ -75,5 +159,10 @@ function formData(){
   }
 }
 
+function clearForm(){
+  document.getElementById("form").reset();
+}
 
-
+function clear(){
+  document.getElementById("answer").reset();
+}
